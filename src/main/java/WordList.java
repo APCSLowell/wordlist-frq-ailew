8bad0f136ca84c2d4357f8dad7d6a9c9a8cd7fd1
ailew <143014764+ailew@users.noreply.github.com>
2024-04-20 02:08:54 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -1,33 +1,36 @@
 import java.util.*;
 
-public class WordList
-{
-  private ArrayList <String> myList;
+public class WordList {
+    private ArrayList<String> myList;
 
-  public WordList() { myList = new ArrayList<String>(); }
-  public void add(String word) { myList.add(word); }
-  public String toString() { return myList.toString(); }
-
-  public int numWordsOfLength(int len)
-  {
-    int result = 0;
-    for(int i = 0; i< wordList.length();i++){
-      if(i.length==len){
-        result++;
-      }
-      return result;
-    }
-    
-  }
-
-  public void removeWordsOfLength(int len)
-  {
-    for(int i = 0; i<wordList.length();i++){
-      if(i.length==len){
-        wordList.remove(i);
-        i++;
-      }
+    public WordList() {
+        myList = new ArrayList<String>();
     }
 
-  }
+    public void add(String word) {
+        myList.add(word);
+    }
+
+    public String toString() {
+        return myList.toString();
+    }
+
+    public int numWordsOfLength(int len) {
+        int result = 0;
+        for (int i = 0; i < myList.size(); i++) {
+            if (myList.get(i).length() == len) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public void removeWordsOfLength(int len) {
+        for (int i = 0; i < myList.size(); i++) {
+            if (myList.get(i).length() == len) {
+                myList.remove(i);
+                i--; // Adjust index after removal
+            }
+        }
+    }
 }
